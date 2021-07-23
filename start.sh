9cc7186f6fc50dbfd7e738fe2cc78bb744350338
atomatoe <enikeev08@list.ru>
2021-07-23 19:50:59 +0300

    done

---
+++ start.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+
+mkdir -p target
+find * -name "*.java" > sources.txt
+javac -d target @sources.txt
+jar -cmf src/manifest.txt avaj-launcher.jar -C target .
+java -jar avaj-launcher.jar scenario.txt
